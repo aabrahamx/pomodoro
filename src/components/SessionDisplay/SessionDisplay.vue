31d509e9fc6ed65af85696425fde47a3e44289a3
Abel A <abelabrahamx@gmail.com>
2022-04-05 23:38:35 -0500

    add: new display component

---
+++ src/components/SessionDisplay/SessionDisplay.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    session: String
+});
+
+let expression;
+switch(props.session) {
+    case 'finished':
+        expression = 'yay! way to go! 👏🏼';
+        break;
+    case 'work':
+        expression = 'work ⏰';
+        break;
+    case 'short':
+        expression = 'short break 🧘🏽‍♂️';
+        break;
+    case 'long':
+        expression = 'long break 🏖'
+        break;
+    default: 
+        expression = 'ready to work?'
+}
+</script>
+
+<template>
+    <div class="wrapper">
+        <span>{{ expression }}</span>
+    </div>
+</template>
+
+<style scoped>
+.wrapper {
+    width: 100%;
+    padding: 5px 3px;
+    background-color: rgba(0, 0, 0, 0.2);
+}
+</style>
