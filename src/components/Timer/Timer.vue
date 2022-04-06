d33e7a9d19b2e4cfebb12a64093d8a977c187b38
Abel A <abelabrahamx@gmail.com>
2022-04-05 23:40:07 -0500

    change: refactor

--- src/components/Timer/Timer.vue
+++ src/components/Timer/Timer.vue
@@ -1,3 +1,119 @@
+<script setup>
+import { reactive } from "vue";
+import SessionDisplay from '../SessionDisplay/SessionDisplay.vue'
+import {
+    displayElement,
+    hideElement,
+    playNotification,
+    displaySession,
+} from "../../helpers/dom.js";
+import {
+    changeMinuteToSecond,
+    formattedTime,
+} from "../../helpers/time-formatters.js";
+
+const timer = reactive({
+    // --- mutated by user ---
+    focusTime: 0.1,
+    shortBreak: 0.2,
+    longBreak: 0.5,
+    numberOfSessions: 4,
+
+    // --- mutated internally ---
+    time: null,
+    display: "00 : 00",
+    isRunning: false,
+    isPaused: false,
+    pausedRemainingTime: null,
+    currentSession: 0,
+    sessionOver: false,
+    focusTimeTurn: true,
+    shortBreakTurn: false,
+    longBreakTurn: false,
+    sessionTurn: "work",
+    sessionRunning: "default",
+});
+
+const counter = {
+    start(functionToBeExecuted) {
+        this.setIntervalID = setInterval(functionToBeExecuted, 1000);
+    },
+    stop() {
+        clearInterval(this.setIntervalID);
+    },
+};
+function handleReset() {
+    counter.stop();
+    reset();
+}
+function setNextTurn(currentRunningTurn) {
+    const all = timer.numberOfSessions;
+    const current = timer.currentSession;
+    const done = all - current === 0;
+    if (!done) {
+        currentRunningTurn === "work"
+            ? (timer.sessionTurn = "short")
+            : (timer.sessionTurn = "work");
+    } else if (done) {
+        timer.sessionTurn = "long";
+    }
+}
+function reset() {
+    (timer.time = null), (timer.display = "00 : 00");
+    timer.isRunning = false;
+    timer.isPaused = false;
+    timer.pausedRemainingTime = null;
+    timer.currentSession = 0;
+    timer.sessionOver = false;
+    timer.sessionTurn = "work";
+    timer.sessionRunning = "default";
+}
+function sessionRunner() {
+    timer.time = sessionPicker();
+    counter.start(countDown);
+}
+function countDown() {
+    timer.display = formattedTime(timer.time);
+    timer.time === 0 ? timerManager() : timer.time--;
+}
+function timerManager() {
+    counter.stop();
+    timer.sessionOver ? handleReset() : sessionRunner();
+}
+function handleStart() {
+    timer.isPaused = false;
+    timer.isRunning = true;
+    sessionRunner();
+}
+function handlePause() {
+    counter.stop();
+    timer.isPaused = true;
+    timer.isRunning = false;
+    timer.pausedRemainingTime = timer.time;
+}
+function sessionPicker() {
+    if (timer.pausedRemainingTime) {
+        const PausedTimeAt = timer.pausedRemainingTime;
+        timer.pausedRemainingTime = null;
+        return PausedTimeAt;
+    } else {
+        if (timer.sessionTurn === "work") {
+            timer.sessionRunning = "work";
+            timer.currentSession++;
+            setNextTurn("work");
+            return changeMinuteToSecond(timer.focusTime);
+        } else if (timer.sessionTurn === "short") {
+            timer.sessionRunning = "short";
+            setNextTurn("short");
+            return changeMinuteToSecond(timer.shortBreak);
+        } else if (timer.sessionTurn === "long") {
+            timer.sessionRunning = "long";
+            timer.sessionOver = true;
+            return changeMinuteToSecond(timer.longBreak);
+        }
+    }
+}
+</script>
 
 <template>
     <div id="session-wrapper">
@@ -43,12 +159,6 @@
     <div id="timer-wrapper">
         <div>
             <div id="timer">
-                <div>
-                    <span id="session"></span>git add REAM
-                    <span v-if="(timer.sessionRunning === 'work')">Work Session ⏰</span>
-                    <span v-if="timer.sessionRunning === 'short'">Short Break 🧘🏽</span>
-                    <span v-if="timer.sessionRunning === 'long'">Long Break 🏖</span>
-                </div>
                 <div id="time-holder">
                     <span>{{ timer.display }}</span>
                 </div>
@@ -59,6 +169,7 @@
             </div>
         </div>
     </div>
+    <SessionDisplay :session="timer.sessionRunning" />
     <div id="btn-wrapper">
         <div>
             <button id="reset" @click="handleReset">Reset</button>
@@ -79,119 +190,6 @@
         <source src="../assets/audio/Ticking.mp3" />
     </audio>
 </template>
-
-
-<script setup>
-
-import { reactive } from 'vue';
-import { displayElement, hideElement, playNotification, displaySession } from '../../helpers/dom.js';
-import { changeMinuteToSecond, formattedTime } from '../../helpers/time-formatters.js';
-
-const timer = reactive({
-    // --- mutated by user ---
-    focusTime: .10,
-    shortBreak: 0.2,
-    longBreak: .5,
-    numberOfSessions: 4,
-
-    // --- mutated internally ---
-    time: null,
-    display: '00 : 00',
-    isRunning: false,
-    isPaused: false,
-    pausedRemainingTime: null,
-    currentSession: 0,
-    sessionOver: false,
-    focusTimeTurn: true,
-    shortBreakTurn: false,
-    longBreakTurn: false,
-    sessionTurn: 'work',
-    sessionRunning: 'default'
-});
-
-
-const counter = {
-    start(functionToBeExecuted) {
-        this.setIntervalID = setInterval(functionToBeExecuted, 1000);
-    },
-    stop() {
-        clearInterval(this.setIntervalID);
-    }
-}
-function handleReset() {
-    counter.stop()
-    reset()
-}
-function setNextTurn(currentRunningTurn) {
-    const all = timer.numberOfSessions;
-    const current = timer.currentSession;
-    const done = (all - current === 0)
-    if (!done) {
-        currentRunningTurn === 'work' 
-        ? timer.sessionTurn = 'short' 
-        : timer.sessionTurn = 'work'
-    } else if(done) {
-        timer.sessionTurn = 'long'
-    }
-}
-function reset() {
-    timer.time = null,
-    timer.display = '00 : 00';
-    timer.isRunning = false;
-    timer.isPaused = false;
-    timer.pausedRemainingTime = null;
-    timer.currentSession = 0;
-    timer.sessionOver = false;
-    timer.sessionTurn = 'work';
-    timer. sessionRunning = 'default'
-}
-function sessionRunner() {
-    timer.time = sessionPicker();
-    counter.start(countDown);
-}
-function countDown() {
-    timer.display = formattedTime(timer.time);
-    timer.time === 0 ? timerManager() : timer.time--;
-}
-function timerManager() {
-    counter.stop()
-    timer.sessionOver ? handleReset() : sessionRunner()
-}
-function handleStart() {
-    timer.isPaused = false;
-    timer.isRunning = true;
-    sessionRunner()
-}
-function handlePause() {
-    counter.stop();
-    timer.isPaused = true;
-    timer.isRunning = false;
-    timer.pausedRemainingTime = timer.time;
-}
-function sessionPicker() {
-    if (timer.pausedRemainingTime) {
-        const PausedTimeAt = timer.pausedRemainingTime;
-        timer.pausedRemainingTime = null;
-        return PausedTimeAt
-    } else {
-        if (timer.sessionTurn === 'work') {
-            timer.sessionRunning = 'work';
-            timer.currentSession++
-            setNextTurn('work');
-            return changeMinuteToSecond(timer.focusTime);
-        } else if (timer.sessionTurn === 'short') {
-            timer.sessionRunning = 'short';
-            setNextTurn('short');
-            return changeMinuteToSecond(timer.shortBreak);
-        } else if (timer.sessionTurn === 'long') {
-            timer.sessionRunning = 'long'
-            timer.sessionOver = true;
-            return changeMinuteToSecond(timer.longBreak);
-        }
-    }
-}
-</script>
-
 
 <style scoped>
 * {
