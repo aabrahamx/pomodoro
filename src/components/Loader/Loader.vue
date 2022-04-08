39368592d2db38edfcbb798a70dfb9dcdffd6051
Abel A <abelabrahamx@gmail.com>
2022-04-08 11:43:49 -0500

    new component

---
+++ src/components/Loader/Loader.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    height: Number,
+});
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <div :style="{height: props.height + '%'}"></div>
+    </div>
+</template>
+
+<style scoped>
+    .wrapper {
+        width: 240px;
+        height: 240px;
+        border-radius: 50%;
+        position: fixed;
+        display: flex;
+        justify-content: flex-end;
+        overflow: hidden;
+        border: 1px solid #05386b;
+    }
+    .wrapper > div {
+        background-color: #05386b;
+        height: 100%;
+        width: 100%;
+
+    }
+</style>
---
+++ src/components/TimeDisplay/TimeDisplay.vue
@@ -0,0 +1,53 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        timer: String,
+        currentSession: Number,
+        numberOfSessions: Number,
+    });
+</script>
+
+<template>
+    <div class="wrapper">
+        <div id="timer">
+            <div id="time-holder">
+                <span>{{ props.timer }}</span>
+            </div>
+
+            <div>
+                <span>{{ props.currentSession }} / {{ props.numberOfSessions }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.wrapper {
+    width: 240px;
+    height: 240px;
+}
+
+.wrapper > div {
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+}
+#timer {
+    width: 240px;
+    height: 240px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+#time-holder {
+    font-size: 1.5rem;
+}
+#timer > div {
+    margin: 5px;
+}
+</style>
