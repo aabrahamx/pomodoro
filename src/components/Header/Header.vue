b730acf1dc3c3515365a5b015a01edf9b3f78242
Abel A <abelabrahamx@gmail.com>
2022-04-10 16:17:00 -0500

    style change

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,38 +3,18 @@
 </script>
 
 <template>
-    <div class="wrapper">
-        <span>Pomodoro Timer</span>
-        <button>
-            <img src="https://img.icons8.com/ios-filled/50/ffffff/do-not-disturb-2.png"/>
-            <img hidden src="https://img.icons8.com/ios-filled/50/ffffff/sun--v1.png"/>
-        </button>
-    </div>
+    <header class="wrapper-header">
+        <h4>Pomodoro Timer</h4>
+    </header>
 </template>
 
 <style scoped>
-.wrapper {
+.wrapper-header {
+    height: 5vh;
     width: 100%;
     display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-direction: row;
-}
-.wrapper > button {
-    margin: 0;
-    padding: 0;
-    background-color: rgba(0, 0, 0, .2);
-    border: none;
-    border-radius: 40%;
-    height: 1.5rem;
-    width: 1.75rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-}
-.wrapper > button > img {
-    height: 50%;
-    width: 50%;
+    align-items: flex-start;
+    justify-content: flex-start;
+    color: #05386b;
 }
 </style>
