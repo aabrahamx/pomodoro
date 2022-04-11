7133a6303266e3c5a2c12a8e7fbc84abb327f761
Abel A <abelabrahamx@gmail.com>
2022-04-10 21:28:50 -0500

    first commit

---
+++ src/helpers/colorStyle.js
@@ -0,0 +1,7 @@
+export const colors = {
+  pri: { name: "emerald green", hex: "#5CDB95" },
+  sec: { name: "dark imperial blue", hex: "#05386B" },
+  ter: { name: "beige", hex: "#EDF5E1" },
+  quat: { name: "dark green", hex: "#379683" },
+  quin: { name: "light green", hex: "#8EE4AF" },
+};
