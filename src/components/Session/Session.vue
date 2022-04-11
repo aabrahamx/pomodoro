e13162e64256ec30f07dc798e4c8a65bbe97a0f5
Abel A <abelabrahamx@gmail.com>
2022-04-10 20:16:12 -0500

    component build

--- src/components/Session/Session.vue
+++ src/components/Session/Session.vue
@@ -1,26 +1,27 @@
 <script setup>
-import { defineProps } from 'vue';
+  import { defineProps } from "vue";
 
-const props = defineProps({
-    session: String
-});
-
+  const props = defineProps({
+    session: String,
+  });
 </script>
 
 <template>
-    <div class="wrapper">
-        <span v-if="props.session === 'default'">Ready to work?</span>
-        <span v-else-if="props.session === 'finished'">Yay! Way to go! 👏🏼</span>
-        <span v-else-if="props.session === 'work'">Work ⏰</span>
-        <span v-else-if="props.session === 'short'">Short Break 🧘🏽‍♂️</span>
-        <span v-else-if="props.session === 'long'">Long Break 🏖</span>
-    </div>
+  <div class="wrapper-session">
+    <span v-if="props.session === 'default'">Ready to work?</span>
+    <span v-else-if="props.session === 'finished'">Yay! Way to go! 👏🏼</span>
+    <span v-else-if="props.session === 'work'">Work ⏰</span>
+    <span v-else-if="props.session === 'short'">Short Break 🧘🏽‍♂️</span>
+    <span v-else-if="props.session === 'long'">Long Break 🏖</span>
+  </div>
 </template>
 
 <style scoped>
-.wrapper {
+  .wrapper-session {
     width: 100%;
     padding: 5px 3px;
+    text-align: center;
+    color: #edf5e1;
     background-color: rgba(0, 0, 0, 0.2);
-}
+  }
 </style>
