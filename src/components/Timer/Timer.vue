efcb1957cd693dc1bcb87eda009b70bf2c428cac
Abel A <abelabrahamx@gmail.com>
2022-04-10 21:33:01 -0500

    refactor

--- src/components/Timer/Timer.vue
+++ src/components/Timer/Timer.vue
@@ -1,30 +1,35 @@
 <script setup>
-import { reactive } from "vue";
-import SessionDisplay from '../SessionDisplay/SessionDisplay.vue';
-import UnloadingDisplay from "../UnloadingDisplay/UnloadingDisplay.vue";
-import {
-    displayElement,
-    hideElement,
-    playNotification,
-    displaySession,
-} from "../../helpers/dom.js";
-import {
+  import { reactive, ref } from "vue";
+  import { storeToRefs } from "pinia";
+  import { useTimer } from "../../app/store";
+
+  import Session from "../Session/Session.vue";
+  import Loader from "../Loader/Loader.vue";
+  import TimeDisplay from "../TimeDisplay/TimeDisplay.vue";
+  import Setting from "../Setting/Setting.vue";
+  import Task from "../Task/Task.vue";
+
+  import {
     changeMinuteToSecond,
     formattedTime,
-} from "../../helpers/time-formatters.js";
+  } from "../../helpers/time-formatters.js";
 
-const timer = reactive({
+  const store = useTimer();
+  const { workSession, shortBreak, longBreak, sessionAmount } =
+    storeToRefs(store);
+
+  const timer = reactive({
     // --- mutated by user ---
-    focusTime: 0.1,
-    shortBreak: 0.2,
-    longBreak: 0.5,
-    numberOfSessions: 4,
+    focusTime: workSession,
+    shortBreak: shortBreak,
+    longBreak: longBreak,
+    numberOfSessions: sessionAmount,
 
     // --- mutated internally ---
     remainingTime: null,
     startingTime: null,
 
-    display: "00 : 00",
+    display: "25 : 00",
     isRunning: false,
     isPaused: false,
     pausedRemainingTime: null,
@@ -35,35 +40,35 @@ const timer = reactive({
     longBreakTurn: false,
     sessionTurn: "work",
     sessionRunning: "default",
-});
+  });
 
-const counter = {
+  const counter = {
     start(functionToBeExecuted) {
-        this.setIntervalID = setInterval(functionToBeExecuted, 1000);
+      this.setIntervalID = setInterval(functionToBeExecuted, 1000);
     },
     stop() {
-        clearInterval(this.setIntervalID);
+      clearInterval(this.setIntervalID);
     },
-};
-function handleReset() {
+  };
+  function handleReset() {
     counter.stop();
     reset();
-}
-function setNextTurn(currentRunningTurn) {
+  }
+  function setNextTurn(currentRunningTurn) {
     const all = timer.numberOfSessions;
     const current = timer.currentSession;
     const done = all - current === 0;
     if (!done) {
-        currentRunningTurn === "work"
-            ? (timer.sessionTurn = "short")
-            : (timer.sessionTurn = "work");
+      currentRunningTurn === "work"
+        ? (timer.sessionTurn = "short")
+        : (timer.sessionTurn = "work");
     } else if (done) {
-        timer.sessionTurn = "long";
+      timer.sessionTurn = "long";
     }
-}
-function reset() {
+  }
+  function reset() {
     timer.remainingTime = null;
-    timer.startingTime = null
+    timer.startingTime = null;
     timer.display = "00 : 00";
     timer.isRunning = false;
     timer.isPaused = false;
@@ -72,228 +77,141 @@ function reset() {
     timer.sessionOver = false;
     timer.sessionTurn = "work";
     timer.sessionRunning = "default";
-}
-function sessionRunner() {
+  }
+  function sessionRunner() {
     timer.remainingTime = sessionPicker();
     timer.startingTime = timer.remainingTime;
     counter.start(countDown);
-}
-function countDown() {
+  }
+  function countDown() {
     timer.display = formattedTime(timer.remainingTime);
     timer.remainingTime === 0 ? timerManager() : timer.remainingTime--;
-}
-function timerManager() {
+  }
+  function timerManager() {
     counter.stop();
     timer.sessionOver ? handleReset() : sessionRunner();
-}
-function handleStart() {
+  }
+  function handleStart() {
     timer.isPaused = false;
     timer.isRunning = true;
     sessionRunner();
-}
-function handlePause() {
+  }
+  function handlePause() {
     counter.stop();
     timer.isPaused = true;
     timer.isRunning = false;
     timer.pausedRemainingTime = timer.remainingTime;
-}
-function sessionPicker() {
+  }
+  function sessionPicker() {
     if (timer.pausedRemainingTime) {
-        const PausedTimeAt = timer.pausedRemainingTime;
-        timer.pausedRemainingTime = null;
-        return PausedTimeAt;
+      const PausedTimeAt = timer.pausedRemainingTime;
+      timer.pausedRemainingTime = null;
+      return PausedTimeAt;
     } else {
-        if (timer.sessionTurn === "work") {
-            timer.sessionRunning = "work";
-            timer.currentSession++;
-            setNextTurn("work");
-            return changeMinuteToSecond(timer.focusTime);
-        } else if (timer.sessionTurn === "short") {
-            timer.sessionRunning = "short";
-            setNextTurn("short");
-            return changeMinuteToSecond(timer.shortBreak);
-        } else if (timer.sessionTurn === "long") {
-            timer.sessionRunning = "long";
-            timer.sessionOver = true;
-            return changeMinuteToSecond(timer.longBreak);
-        }
+      if (timer.sessionTurn === "work") {
+        timer.sessionRunning = "work";
+        timer.currentSession++;
+        setNextTurn("work");
+        return changeMinuteToSecond(timer.focusTime);
+      } else if (timer.sessionTurn === "short") {
+        timer.sessionRunning = "short";
+        setNextTurn("short");
+        return changeMinuteToSecond(timer.shortBreak);
+      } else if (timer.sessionTurn === "long") {
+        timer.sessionRunning = "long";
+        timer.sessionOver = true;
+        return changeMinuteToSecond(timer.longBreak);
+      }
     }
-}
+  }
 </script>
 
 <template>
-    <div id="session-wrapper">
-        <label for="sessionNumber">Sessions</label>
-        <input
-            v-model="timer.numberOfSessions"
-            type="number"
-            id="sessionNumber"
-            min="1"
-            max="10"
-            step="1"
+  <main class="wrapper-main">
+    <section class="container-top">
+      <Setting />
+      <Task />
+    </section>
+
+    <section class="container-main">
+      <div>
+        <Loader :height="(timer.remainingTime / timer.startingTime) * 100" />
+        <TimeDisplay
+          :timer="timer.display"
+          :current-session="timer.currentSession"
+          :number-of-sessions="timer.numberOfSessions"
         />
-    </div>
-    <div id="setting-wrapper">
-        <div>
-            <label for="focus">Work</label>
-            <input v-model="timer.focusTime" type="number" min="5" max="120" step="5" id="focus" />
-        </div>
-        <div>
-            <label for="short-break">Short Break</label>
-            <input
-                v-model="timer.shortBreak"
-                type="number"
-                min="5"
-                max="30"
-                step="5"
-                id="short-break"
-            />
-        </div>
-        <div>
-            <label for="long-break">Long Break</label>
-            <input
-                v-model="timer.longBreak"
-                type="number"
-                min="15"
-                max="120"
-                step="15"
-                id="long-break"
-            />
-        </div>
-    </div>
+      </div>
+    </section>
 
-    <div id="timer-wrapper">
-        <UnloadingDisplay :starting="timer.startingTime" :remaining="timer.remainingTime" />
-        <div>
-            <div id="timer">
-                <div id="time-holder">
-                    <span>{{ timer.display }}</span>
-                </div>
+    <hr class="line-break" />
 
-                <div>
-                    <span>{{ timer.currentSession }} / {{ timer.numberOfSessions }}</span>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr class="line-break">
-    <SessionDisplay :session="timer.sessionRunning" />
-    <div id="btn-wrapper">
-        <div>
-            <button id="reset" @click="handleReset">Reset</button>
-        </div>
-        <div>
-            <div id="start-btn">
-                <button v-if="!timer.isRunning" @click="handleStart">Start</button>
-            </div>
-            <div id="pause-btn">
-                <button v-if="timer.isRunning" @click="handlePause">Pause</button>
-            </div>
-        </div>
-    </div>
-    <audio id="audio">
-        <source src="../assets/audio/positive-notification.mp3" type="audio/mp3" />
-    </audio>
-    <audio id="tickingAudio">
-        <source src="../assets/audio/Ticking.mp3" />
-    </audio>
+    <section class="container-bottom">
+      <Session :session="timer.sessionRunning" />
+      <div class="container-btn">
+        <button class="btn" @click="handleReset">Reset</button>
+        <button class="btn" @click="handleStart" v-if="!timer.isRunning">
+          Start
+        </button>
+        <button class="btn" @click="handlePause" v-if="timer.isRunning">
+          Pause
+        </button>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-* {
-    color: #edf5e1;
-}
-#timer-wrapper {
+  .wrapper-main,
+  .container-top,
+  .container-bottom,
+  .container-btn {
+    width: 100%;
+    overflow: hidden;
+  }
+  .container-top,
+  .container-main,
+  .container-main > div,
+  .container-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .container-main {
     height: 300px;
     width: 300px;
-    margin: 10px auto;
-    background-color: #05386b;
-    border: 2px solid #8ee4af;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-#timer-wrapper > div {
-    width: 85%;
-    height: 85%;
-    border-radius: 50%;
-    border: 3px solid #8ee4af;
-    background-color: #5cdb95;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-#timer {
-    height: 95%;
-    width: 95%;
+    margin: 2rem auto;
     border-radius: 50%;
     background-color: #05386b;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  }
+  .container-main > div {
+    width: 260px;
+    height: 260px;
+    border-radius: 50%;
     flex-direction: column;
-    border: 2px solid #8ee4af;
-}
-#time-holder {
-    font-size: 1.5rem;
-}
-#timer > div {
-    margin: 5px;
-}
-button {
-    padding: 4px;
+    background-color: #5cdb95;
+  }
+  .btn {
     width: 100px;
     height: 30px;
-    border: 0px solid #05386b;
-    color: #05386b;
+    padding: 4px;
+    margin: 10px 15px;
+    border: none;
     font-weight: bolder;
-    background-color: #8ee4af;
-    transition: box-shadow .3s linear;
-}
-button:active,
-button:hover {
-    box-shadow: -2px 2px 0px 0px #05386b;
-    border: 1px solid #05386b;
-}
-
-/* setting styles */
-input {
-    height: 20px;
-    width: 50px;
-    margin: 5px;
-    text-align: center;
     color: #05386b;
-    border: none;
-    background-color: #379683;
-}
-#setting-wrapper {
-    display: flex;
-}
-#setting-wrapper > div {
-    margin: 0 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-end;
-}
-#session-wrapper label,
-#session-wrapper input {
-    display: block;
-    margin: 0.25rem auto;
-    width: 270px;
-}
-#btn-wrapper {
-    display: flex;
-}
-#btn-wrapper > div {
-    margin: 1rem 0.5rem;
-}
-
-.line-break {
+    background-color: #8ee4af;
+    transition: box-shadow 0.3s linear;
+  }
+  .btn:active,
+  .btn:hover {
+    border: 1px solid #05386b;
+    box-shadow: -2px 2px 0px 0px #05386b;
+  }
+  .line-break {
     width: 100%;
-    height: 2px;
-    background-color: black;
+    height: 1px;
     border: none;
-}
+    background-color: rgba(0, 0, 0, 0.9);
+  }
 </style>
