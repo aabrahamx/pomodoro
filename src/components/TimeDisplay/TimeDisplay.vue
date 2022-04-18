b1ccb1cd985cbbd7a506d1b39109fdcceed0745f
Abel A <abelabrahamx@gmail.com>
2022-04-18 13:19:04 -0500

    pointer animation style add

--- src/components/TimeDisplay/TimeDisplay.vue
+++ src/components/TimeDisplay/TimeDisplay.vue
@@ -11,11 +11,27 @@ const props = defineProps({
 <template>
     <div class="wrapper-timer">
         <div class="container-timer">
-            <div class="timer">
-                <span>{{ props.timer }}</span>
+            <div>
+                <span class="time-holder">{{ props.timer }}</span>
             </div>
             <div>
-                <span>{{ props.currentSession }} / {{ props.numberOfSessions }}</span>
+                <span class="session-holder">{{ props.currentSession }} / {{ props.numberOfSessions }}</span>
+            </div>
+        </div>
+        <div class="container-animation">
+            <div class="top">
+                <div class="pointer"></div>
+            </div>
+            <div class="center">
+                <div class="left">
+                    <div class="pointer"></div>
+                </div>
+                <div class="right">
+                    <div class="pointer"></div>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="pointer"></div>
             </div>
         </div>
     </div>
@@ -39,7 +55,61 @@ const props = defineProps({
     border-radius: 50%;
 }
 
-.timer {
-    font-size: 1.5rem;
+.container-timer>div {
+    margin: .15rem auto;
 }
+
+.time-holder {
+    font-size: 1.85rem;
+    font-weight: bold;
+}
+
+
+.container-animation {
+    width: 240px;
+    height: 240px;
+    position: fixed;
+    z-index: 10;
+    border-radius: 50%;
+    transform: rotate(45deg);
+    overflow: hidden;
+    padding: 1rem;
+}
+.container-animation > div {
+    height: 33.33%;
+}
+.center {
+    display: flex;
+}
+.center > div {
+    width: 50%;
+    height: 100%;
+}
+
+.pointer {
+    background: #5cdb95;
+    border-radius: 10px;
+}
+.top > .pointer,
+.bottom > .pointer {
+    height: 20px;
+    width: 6px;
+}
+.left > .pointer,
+.right > .pointer {
+    width: 20px;
+    height: 6px;
+}
+
+.top, .left, .right, .bottom {
+    display: flex;
+}
+.left, .right {align-items: center;}
+.top, .bottom {justify-content: center;}
+.left {justify-content: flex-start;}
+.right {justify-content: flex-end;}
+.top {align-items: flex-start;}
+.bottom {align-items: flex-end;}
+
+
 </style>
