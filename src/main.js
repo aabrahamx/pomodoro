1398b9c328689b040a79891f3aac2006ba3ecf2f
Abel A <abelabrahamx@gmail.com>
2022-05-24 00:53:47 -0500

    tailwind integration

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './app/App.vue'
 import './registerServiceWorker'
+import './index.css'
 
 const app = createApp(App);
 app.use(createPinia());
