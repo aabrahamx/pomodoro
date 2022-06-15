a3104ece8c99b76f9eb9c729fa1bdcca362f6318
Abel A <abelabrahamx@gmail.com>
2022-06-15 17:06:57 -0500

    add skip next feature

--- src/components/Controls/Controls.vue
+++ src/components/Controls/Controls.vue
@@ -4,19 +4,25 @@
   import playIcon from '@/assets/icons/glyph-play-90.png';
   import pauseIcon from '@/assets/icons/glyph-pause-90.png';
   import resetIcon from '@/assets/icons/glyph-reset-90.png';
+  import skipIcon from '@/assets/icons/glyph-skip-90.png';
 
   const props = defineProps({
     running: {
       type: Boolean,
       required: true,
     },
+    currentSession: {
+      type: String,
+      required: true.valueOf,
+    },
   });
   const { running } = toRefs(props);
 
-  const emit = defineEmits(['start', 'pause', 'reset']);
+  const emit = defineEmits(['start', 'pause', 'reset', 'skip']);
   const handleStart = () => emit('start');
   const handlePause = () => emit('pause');
   const handleReset = () => emit('reset');
+  const handleSkip = () => emit('skip');
 </script>
 
 <template>
@@ -42,6 +48,14 @@
       class="mx-5 hover:opacity-70 transition-opacity"
     >
       <img :src="pauseIcon" alt="pause icon" class="h-8 pointer-events-none" />
+    </button>
+
+    <button
+      :disabled="currentSession === 'long' || !running"
+      @click="handleSkip"
+      class="mx-5 hover:opacity-70 transition-opacity scale-110"
+    >
+      <img :src="skipIcon" alt="skip icon" class="h-8 pointer-events-none" />
     </button>
   </section>
 </template>
