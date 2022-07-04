56fcb6f0ae2492784b1b46853d32355ed51021e9
Abel A <abelabrahamx@gmail.com>
2022-07-04 18:36:16 -0500

    fix: build error

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Pomodoro from '@/views/Pomodoro.vue'
-import settings from '@/views/settings.vue'
+import Settings from '@/views/Settings.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +14,7 @@ const router = createRouter({
     {
       path: '/settings',
       name: 'settings',
-      component: settings,
+      component: Settings,
     },
   ],
   linkActiveClass: 'active',
